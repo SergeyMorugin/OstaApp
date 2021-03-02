4f355d1a9f4c57ae897043011c68ee6ecb31d07a
Sergei Morugin <cmorugin@gmail.com>
2021-03-02 16:11:39 +0300

    add FeedController

---
+++ ostagram_v1/FeedViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewCell.swift
+//  ostagram_v1
+//
+//  Created by Matthew on 02.03.2021.
+//  Copyright © 2021 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
